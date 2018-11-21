4c26bf03c4371741b9bc7e602390b0c1f9fbd089
bendgk <benkosten@gmail.com>
2018-11-21 07:43:03 -0600

    Add image of triangle

--- src/main.py
+++ src/main.py
@@ -18,6 +18,9 @@ class Window:
         glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
         glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
         glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        
+        #only for macOS
+        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
 
         self.glfw_window = glfw.create_window(self.x, self.y, self.title, None, None)
         if not self.glfw_window:
@@ -33,24 +36,44 @@ class Window:
         self.ctx = moderngl.create_context()
 
         self.prog = self.ctx.program(
-            vertex_shader = """
+#            vertex_shader = """
+#                #version 330
+#
+#                in vec3 in_vert;
+#
+#                void main() {
+#                    gl_Position = vec4(in_vert, 1.0);
+#                }
+#            """,
+#            fragment_shader = """
+#                #version 330
+#
+#                out vec4 f_color;
+#
+#                void main() {
+#                    f_color = vec4(1, 1, 1, 1);
+#                }
+#            """
+            vertex_shader='''
                 #version 330
-
-                in vec3 in_vert;
-
+                in vec2 in_vert;
+                in vec3 in_color;
+                out vec3 v_color;    // Goes to the fragment shader
                 void main() {
-                    gl_Position = vec4(in_vert, 1.0);
+                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                    v_color = in_color;
                 }
-            """,
-            fragment_shader = """
+            ''',
+            fragment_shader='''
                 #version 330
-
+                in vec3 v_color;
                 out vec4 f_color;
-
                 void main() {
-                    f_color = vec4(1, 1, 1, 1);
+                    // We're not interested in changing the alpha value
+                    f_color = vec4(v_color, 1.0);
                 }
-            """)
+            '''
+            )
 
     def framebuffer_size_callback(self, window, width, height):
         self.on_draw()
@@ -73,22 +96,39 @@ class Window:
 
 if __name__ == '__main__':
     window = Window([640, 480], "pyengine-demo", None, None)
-
+    
+    """
     points = np.array([
         -0.5,  -0.5,  0,
         0.5, -0.5,  0,
         0.5, 0.5,  0,
         -0.5, 0.5, 0
     ])
+    """
+    
+    vertices = np.array([
+            # x, y, red, green, blue
+            0.0, 0.8, 1.0, 0.0, 0.0,
+            -0.6, -0.8, 0.0, 1.0, 0.0,
+            0.6, -0.8, 0.0, 0.0, 1.0,
+        ])
 
     while not glfw.window_should_close(window.glfw_window):
         #moderngl stuff
+        
+        """
         ibo = window.ctx.buffer(np.asarray([0, 1, 2, 0, 2, 3]).astype('i4').tobytes())
         vbo = window.ctx.buffer(points.astype('f4').tobytes())
 
         vao_content = [(vbo, '3f', 'in_vert')]
         vao = window.ctx.vertex_array(window.prog, vao_content, ibo)
+        """
+        
+        vbo = window.ctx.buffer(vertices.astype('f4').tobytes())
 
+        # We control the 'in_vert' and `in_color' variables
+        vao = window.ctx.simple_vertex_array(window.prog, vbo, 'in_vert', 'in_color')
+        
         window.on_input()
 
         window.on_draw()
